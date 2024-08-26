572c3ca6f28e25a22840b3db5906dca137697b76
firmanms <firmanmuhamadsahidin@gmail.com>
2024-08-27 00:16:43 +0700

    frontend

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.layouts.app');
 });
 Route::get('formulirruanga/{id}', [App\Http\Controllers\FormulirController::class, 'formulirruanga'])->name('formulirruanga');
