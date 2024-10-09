c4e0f15aaba761331d036e3ccec724413d70d393
DESKTOP-NHNBUA2\Firman <firmanmuhamadsahidin@gmail.com>
2024-10-09 14:07:58 +0700

    91024ol

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 //seting apabila notfound
-//use Livewire\Livewire;
+use Livewire\Livewire;
 
 Route::get('/', function () {
     return view('frontend.layouts.app');
 });
 
 //setting apabila notfound
-//  Livewire::setScriptRoute(function ($handle) {
-//  return Route::get('/kerudung/livewire/livewire.js', $handle);
-//    });
+ Livewire::setScriptRoute(function ($handle) {
+ return Route::get('/kerudung/livewire/livewire.js', $handle);
+   });
 
-//    Livewire::setUpdateRoute(function ($handle) {
-//    return Route::post('/kerudung/livewire/update', $handle);
-//    });
+   Livewire::setUpdateRoute(function ($handle) {
+   return Route::post('/kerudung/livewire/update', $handle);
+   });
 
 Route::get('formulirruangan/{id}', [App\Http\Controllers\FormulirController::class, 'formulirruangan'])->name('formulirruangan');
 Route::get('formulirbangunan1/{id}', [App\Http\Controllers\FormulirController::class, 'formulirbangunan1'])->name('formulirbangunan1');
